663afc44d279b7cefe5b70fa3e4862605272f353
Task Home Challenges <takehomechallenges@gmail.com>
2024-06-23 14:13:29 +0200

    Retornar error para mostrar notification

--- src/components/DayOfTheWeek/index.js
+++ src/components/DayOfTheWeek/index.js
@@ -1,56 +1,104 @@
 // src/DayOfTheWeek.js
 
-import React, { useState } from 'react';
-import { Box, TextField, Button, Typography, Paper } from '@mui/material';
-import { getDayWeek } from '../../helpers/getDayWeek';
-import './style.css';
+import React, { useState } from "react";
+import { Box, TextField, Button, Typography, Paper, Snackbar, Alert } from "@mui/material";
+import { getDayWeek } from "../../helpers/getDayWeek";
+import "./style.css";
 
 const DayOfTheWeek = () => {
-  const [date, setDate] = useState('');
-  const [dayOfWeek, setDayOfWeek] = useState('');
+  const [date, setDate] = useState("");
+  const [dayOfWeek, setDayOfWeek] = useState("");
+  const [userFeedback, setUserFeedback] = useState({
+    open: false,
+    message: "",
+    type: "",
+  });
 
   const handleChange = (event) => {
-    let value = event.target.value.replace(/\D/g, '');
+    let value = event.target.value.replace(/\D/g, "");
     if (value.length > 8) value = value.slice(0, 8);
 
     if (value.length > 4) {
-      value = value.slice(0, 2) + '-' + value.slice(2, 4) + '-' + value.slice(4);
+      value =
+        value.slice(0, 2) + "-" + value.slice(2, 4) + "-" + value.slice(4);
     } else if (value.length > 2) {
-      value = value.slice(0, 2) + '-' + value.slice(2);
+      value = value.slice(0, 2) + "-" + value.slice(2);
     }
 
     setDate(value);
   };
-  
+
   const handleSubmit = () => {
     const day = getDayWeek(date);
-    setDayOfWeek(day);
+    
+    if (day.error) {
+
+      setDayOfWeek('');
+      setUserFeedback({
+        open: true,
+        message: day.error,
+        type: "error",
+      });
+
+    } else {
+
+      setDayOfWeek(day.dayOfWeek);
+
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setUserFeedback({
+      open: false,
+      message: "",
+      type: "",
+    });
   };
 
   return (
-    <Box className="container">
-      <Paper elevation={3} className="paper">
-        <Typography variant="h6" gutterBottom>
-          Introduzca una fecha en el formato DD-MM-AAAA
-        </Typography>
-        <TextField
-          label="Fecha"
-          variant="outlined"
-          value={date}
-          onChange={handleChange}
-          fullWidth
-          className="textField"
-        />
-        <Button variant="contained" onClick={handleSubmit} fullWidth>
-          Obtener día de la semana
-        </Button>
-        {dayOfWeek && (
-          <Typography variant="h6" className="result">
-            Día de la semana: {dayOfWeek}
+    <>
+      <Box className="container">
+        <Paper elevation={3} className="paper">
+          <Typography variant="h6" gutterBottom>
+            Introduzca una fecha en el formato DD-MM-AAAA
           </Typography>
-        )}
-      </Paper>
-    </Box>
+          <TextField
+            label="Fecha"
+            variant="outlined"
+            value={date}
+            onChange={handleChange}
+            fullWidth
+            className="textField"
+          />
+          <Button variant="contained" onClick={handleSubmit} fullWidth>
+            Obtener día de la semana
+          </Button>
+          {dayOfWeek && (
+            <Typography variant="h6" className="result">
+              Día de la semana: {dayOfWeek}
+            </Typography>
+          )}
+        </Paper>
+      </Box>
+      <Snackbar
+        open={userFeedback.open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={userFeedback.type === "success" ? "success" : "error"}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {userFeedback.message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
--- src/helpers/getDayWeek.js
+++ src/helpers/getDayWeek.js
@@ -2,26 +2,23 @@ import { isValidDate, isValidFormat } from './dateValidator';
 
 export function getDayWeek(date) {
   if (!isValidFormat(date)) {
-    alert('La fecha debe tener el formato DD-MM-AAAA');
-    return null;
+    return { error: 'La fecha debe tener el formato DD-MM-AAAA' };
   }
 
   let [day, month, year] = date.split('-').map(Number);
 
   if (year < 1 || year > 99) {
-    alert('El año debe estar entre 1 y 99');
-    return null;
+    return { error: 'El año debe estar entre 1 y 99' };
   }
 
   if (!isValidDate(day, month, year)) {
-    alert('Fecha no válida');
-    return null;
+    return { error: 'Fecha no válida' };
   }
 
   // Asegurar que el año tenga cuatro dígitos
-  year = year + 2000;
+  year += 2000;
 
   const formattedDate = new Date(year, month - 1, day);
   const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-  return daysOfWeek[formattedDate.getDay()];
+  return { dayOfWeek: daysOfWeek[formattedDate.getDay()] };
 }
